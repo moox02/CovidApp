a129d4255bf30aa63432a2add52f53a75493bc74
Nasrul Hakim <moox02@gmail.com>
2022-12-04 22:26:05 +0800

    Include CLAHE and update layout

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from PIL import Image
 import numpy as np
 import tensorflow as tf
 import tensorflow_hub as hub
+import tf_clahe
 import streamlit as st
 
 st.set_option('deprecation.showfileUploaderEncoding', False)
@@ -21,26 +22,37 @@ def load_model():
 if __name__ == '__main__':
 
     model = load_model()
-    st.title('COVID-19 Classifier')
+    st.title('COVID-19 Pneumonia Classifier')
+    st.markdown('##### An Efficient Deep Learning Algorithm For COVID-19 Pneumonia Detection Using Chest Radiography Imaging')
+    st.caption('The algorithm has been trained to distinguish between normal, COVID-19 pneumonia, and other viral or bacterial pneumonia patients. The image will be enhanced using CLAHE technique prior to being passed onto the prediction model.')
     CLASS_NAMES = ['normal', 'pneumonia', 'COVID-19']
 
-    file = st.file_uploader("Upload a chest x-ray to test", type=["jpg", "png"])
+    file = st.file_uploader("Upload a chest x-ray", type=["jpg", "png"])
 
     if file is None:
         st.text('Waiting for upload....')
     else:
 
         test_image = Image.open(file).convert('RGB')
-        st.image(test_image)
 
         image_arr = np.asarray(test_image).astype('float32')
         image = tf.cast(image_arr, tf.float32)
-        image = tf.image.resize(image, [300, 300], antialias=True)/255
-        image = np.expand_dims(image, axis = 0)
+        image = tf.image.resize(image, [300, 300], antialias=True)
+        image_clahe = tf_clahe.clahe(image, tile_grid_size=(7, 7), clip_limit=3.)/255
 
-        pred = model.predict(image)
-        result = CLASS_NAMES[np.argmax(pred)] 
+        col1, col2 = st.columns(2)
+        with col1:
+            st.caption("Uploaded image")
+            st.image(test_image, use_column_width=True)
+        with col2:
+            st.caption("CLAHE")
+            st.image(image_clahe.numpy(), use_column_width=True)
 
-        output = 'This is ' + result + ' patient chest x-ray.'
+        image_clahe = np.expand_dims(image_clahe, axis = 0)
 
-        st.text(output)
+        predictions = model.predict(image_clahe)
+        result = CLASS_NAMES[np.argmax(predictions)]
+        confidence = predictions[0][np.argmax(predictions)]*100
+
+        st.text('The chest x-ray belongs to a ' + result + ' patient.')
+        st.text('Confidence : {:.2f}%'.format(confidence))
